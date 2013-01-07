e0305e34c7ec8b47dfc45784918ce728d39cb33c
Erik C. Grueter <erikgrueter@gmail.com>
2013-01-07 13:29:34 -0500

    flower and polygon to BSS

--- swampy-package/swampy/bss_flower.py
+++ swampy-package/swampy/bss_flower.py
@@ -6,11 +6,18 @@
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
-
-
+bob.delay=.01
+def flower(t,n,length,z):
+	for i in range(z):		 	
+		angle = 360.0 / n
+    		for i in range(n):
+        		fd(t, length)
+        		lt(t, angle)
+		lt(bob,20)
+flower(bob,7,70,20)
 
 # This is where you put code to move bob
-
+# Since i dont have access to math for some reason, I created a flower from the polygon.
 
 
 
--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -1,14 +1,37 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name:Erik Grueter
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
+bob.delay=.01
+# Bob making a square:
+def square(t)
+for i in range(4):
+    fd(t, 100)
+    lt(t)
 
+square(bob)
 
+# Polyline re-factored code
+def polyline(t, n, length, angle):
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
 
+def polygon(t, n, length):
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
+
+# couldnt run this myself. Having problems with math.pi!! 
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
 # This is where you put code to move bob
 
 
